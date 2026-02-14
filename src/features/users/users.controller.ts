fde3cd9aab323eb0c177c56301c2170aecadc511
Rojo Tina <rojotina.radoarivelo@gmail.com>
2026-02-14 13:19:17 +0200

    Getting user info by session id.

--- src/features/users/users.controller.ts
+++ src/features/users/users.controller.ts
@@ -1,13 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 
 import { Users } from './users.type';
 import { CrudController } from '../../core/crud/abstract-crud.controller';
 import { CreateUserDto, UpdateUserDto } from '../../shared/middlewares/DTOs/users.dto';
+import { AuthGuard } from '../../shared/middlewares/Guards/auth.guard';
+import { reponsesDTO } from '../../utils/interfaces/responses';
+import { AuthUserPresenter } from '../../auth/interfaces/presenters/auth-user.presenter';
+import { SignedUserDto } from '../../auth/interfaces/dtos/signed-user.dto';
 
 @Controller('users')
 export class UsersController extends CrudController<CreateUserDto, UpdateUserDto, Users> {
   constructor(service: UsersService) { super(service); }
   // 🔥 Nouvelle route spécifique @Get('active') findActiveUsers() { return []; } } 
+
+  @Get('info')
+  @UseGuards(AuthGuard)
+  async FindByToken(@Req() req: any): Promise<reponsesDTO<SignedUserDto>> {
+    const userPresenter: AuthUserPresenter = new AuthUserPresenter();
+    const dataResponse = req.user ? userPresenter.present(req.user) : null;
+    return {
+      message: 'Information about the user',
+      data: dataResponse,
+      statusCode: 200,
+    };
+  }
 }
